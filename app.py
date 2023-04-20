139309a772ede5dc6555fb7401300244976d7ce8
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2023-04-20 19:21:31 -0400

    try again

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def hello():
     return "<h1 style='color:{}'>Hello World! {}</h1>".format(color, count)
 
 def isEven(n):
-    return n % 2 == 1
+    return n % 2 == 0
 
 
 if __name__ == "__main__":
